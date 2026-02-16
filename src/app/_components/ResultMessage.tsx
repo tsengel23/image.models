19e537cea05a6bb5daf5c3c863242ed99d26aa59
tsengel23 <tsengel0625@gmail.com>
2026-02-16 21:39:36 +0800

    final

--- src/app/_components/ResultMessage.tsx
+++ src/app/_components/ResultMessage.tsx
@@ -4,20 +4,14 @@ import { FileText, LucideIcon } from "lucide-react";
 
 type ResultMessageProps = {
   title: string;
-  // text: string;
   icon?: LucideIcon;
   iconClassName?: string;
-  //   result: string;
-  //   image?: "";
 };
 
 export const ResultMessage = ({
   title,
-  // text,
   icon: Icon = FileText,
   iconClassName = "w-5 h-5 text-orange-400",
-  //   result,
-  //   image,
 }: ResultMessageProps) => {
   return (
     <div className="flex flex-col gap-2 w-full">
@@ -25,8 +19,6 @@ export const ResultMessage = ({
         <Icon className={iconClassName} />
         <h2 className="text-black font-semibold text-xl">{title}</h2>
       </div>
-      {/* <p className="text-[#71717A] font-normal text-sm">{text}</p> */}
-      {/* <div>{result}</div> */}
     </div>
   );
 };
